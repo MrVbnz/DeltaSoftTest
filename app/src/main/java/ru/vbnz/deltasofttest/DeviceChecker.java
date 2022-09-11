e58ca868ac33c3cf406927d9a92dcd15753309ae
MrVbnz <v.vbnz2013@yandex.ru>
2022-09-11 18:23:49 +0500

    More device checks, removed unused .csv file

--- app/src/main/java/ru/vbnz/deltasofttest/DeviceChecker.java
+++ app/src/main/java/ru/vbnz/deltasofttest/DeviceChecker.java
@@ -18,11 +18,24 @@ public class DeviceChecker {
     public Boolean IsUserDevice() {
         if (ContainsGoogle())
             return false;
+        if (isEmulator())
+            return false;
         if (!IsGsm())
             return false;
         if (!HasSim())
             return false;
         return true;
+    }
+
+    public static boolean isEmulator() {
+        return Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || "google_sdk".equals(Build.PRODUCT);
     }
 
     private Boolean IsGsm() {
